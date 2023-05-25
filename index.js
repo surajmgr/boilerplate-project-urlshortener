42e5d8d4a5c2c1feb5dd8b0840a07c650b4ccfac
Suraj Pulami <54809836+surajmgr@users.noreply.github.com>
2023-05-25 03:00:56 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,24 +1,103 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error: "));
+connection.once("open", () => {
+  console.log("Database Connected Successfully!");
+});
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String,
+});
+const URL = mongoose.model("URL", urlSchema);
+
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url;
+  const urlCode = shortid.generate();
+
+  if (!validUrl.isWebUri(url)) {
+    res.json({
+      error: "invalid URL",
+    });
+  } else {
+    try {
+      let findOne = await URL.findOne({
+        original_url: url,
+      });
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url,
+        });
+      } else {
+        findOne = new URL({
+          original_url: url,
+          short_url: urlCode,
+        });
+        await findOne.save();
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json("Server error...");
+    }
+  }
+});
+
+app.get("/api/shorturl/:short_url?", async (req, res) => {
+  try {
+    const urlParams = await URL.findOne({
+      short_url: req.params.short_url,
+    });
+    if (urlParams) {
+      return res.redirect(urlParams.original_url);
+    } else {
+      return res.status(404).json("No URL found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server error...");
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
